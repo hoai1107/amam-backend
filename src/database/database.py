a06f1049b2d792294ad95815663fc15dd69134b0
hoai1107 <khaihoai1107@gmail.com>
2023-02-14 22:28:59 +0700

    Change database from docker to AWS RDS

--- src/database/database.py
+++ src/database/database.py
@@ -1,8 +1,24 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-DATABASE_URL = "postgresql://postgres:123456@localhost:5432/amam"
+load_dotenv()
+
+RDS_POSTGRESQL_URL = os.getenv("RDS_POSTGRESQL_URL")
+RDS_MASTER_USERNAME = os.getenv("RDS_MASTER_USERNAME")
+RDS_MASTER_PASSWORD = os.getenv("RDS_MASTER_PASSWORD")
+
+DATABASE_URL = "postgresql://{username}:{password}@{host}:{port}/{database}".format(
+    username=RDS_MASTER_USERNAME,
+    password=RDS_MASTER_PASSWORD,
+    host=RDS_POSTGRESQL_URL,
+    port=5432,
+    database="amam",
+)
+
+# DATABASE_URL = "postgresql://postgres:123456@localhost:5432/amam"
 
 engine = create_engine(
     DATABASE_URL,
